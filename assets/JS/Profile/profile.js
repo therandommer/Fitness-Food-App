1435a8bfc9dee018f922b73ac33a8fb35ef036dc
Isabella <bdc190799@gmail.com>
2023-11-02 19:48:16 +0000

    Initial Commit
    Added the base variables for each script based on our design document

---
+++ assets/JS/Profile/profile.js
@@ -0,0 +1,19 @@
+//--- Variables ---
+let profilePicture;
+let name = {
+    firstName: "",
+    middleName: "",
+    lastName: ""
+};
+let gender = "";
+let address = "";
+let age = 0;
+let height = 0;
+let weight = 0;
+let BMI = 0;
+let fitnessIntesity = "";
+let selectedExcercises = []; //array of exercise cards to be placed here
+let basket = []; //array of menu items to be placed here
+let totalCaloriesLost = 0; //update based on progress generated from the calendar
+
+//--- Functions ---
