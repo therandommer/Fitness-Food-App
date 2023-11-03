46e5f80d83cf22bc6039efb802804c4a317710cd
michaelfame <michaelfame@gmail.com>
2023-11-03 19:12:29 +0000

    added code for header and navbar, work in progress

--- src/components/hompage/header.js
+++ src/components/hompage/header.js
@@ -1,6 +1,7 @@
 // Header.js
 
 import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/workouts">Workouts</a></li>
+          <li><a href="/login">login</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
