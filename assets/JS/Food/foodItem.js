4476d7ea914100b089dbce01ffa851ab40161aa6
fabiola <fabiola-cute-@hotmail.com>
2023-11-07 19:37:56 +0000

    fabiola

--- assets/JS/Food/foodItem.js
+++ assets/JS/Food/foodItem.js
@@ -10,3 +10,48 @@ let previewImage;
 let largeImage;
 
 //--- Functions ---
+const foodItems = [
+    { name: "Pizza", price: 10.99 },
+    { name: "Burger", price: 8.99 },
+    { name: "Salad", price: 5.99 },
+    { name: "Pasta", price: 12.99 },
+    { name: "Ice Cream", price: 4.99 },
+];
+
+const foodList = document.getElementById("food-items");
+const cartList = document.getElementById("cart-items");
+const totalPrice = document.getElementById("total-price");
+
+foodItems.forEach(item => {
+    const foodItem = document.createElement("li");
+    foodItem.classList.add("food-item");
+    foodItem.innerHTML = `
+        <h2>${item.name}</h2>
+        <p>Price: $${item.price.toFixed(2)}</p>
+        <button onclick="addToCart('${item.name}', ${item.price})">Add to Cart</button>
+    `;
+    foodList.appendChild(foodItem);
+});
+
+let cart = [];
+let cartTotal = 0.0;
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    cartTotal += price;
+
+    const cartItem = document.createElement("li");
+    cartItem.classList.add("cart-item");
+    cartItem.innerHTML = `
+        <span>${name}</span>
+        <span>$${price.toFixed(2)}</span>
+    `;
+    cartList.appendChild(cartItem);
+
+    totalPrice.textContent = `$${cartTotal.toFixed(2)}`;
+}
+
+
+
+
+
