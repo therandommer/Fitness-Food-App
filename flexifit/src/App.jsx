37434b6443ce4b385350e11ac5cf23f9ad0523a4
Isabella <bdc190799@gmail.com>
2023-11-09 15:28:25 +0000

    Fixed dependencies
    Added the remainder of my code, added logic to the navbar to handle logging in and out, fixed typos.

--- flexifit/src/App.jsx
+++ flexifit/src/App.jsx
@@ -5,11 +5,12 @@ import {
   Route,
   Routes,
   BrowserRouter,
+  Navigate,
 } from "react-router-dom";
 import header from "../src/Hompage/header";
 import Navbar from "../src/Hompage/navbar";
 import Home from "./pages/homepage";
-import Login from "./pages/Profile/loginPage";
+import Login from "./pages/Profile/loginPage.jsx";
 import LoginElement from "./pages/Profile/loginElement";
 import Footer from "../src/Hompage/footer";
 import Wrapper from "./components/wrapper";
@@ -26,7 +27,7 @@ function App() {
 
   useEffect(() => {
     //get if the user is logged in within local storage
-    const user = JSON.parse(localStorage.getIteme("user"));
+    const user = JSON.parse(localStorage.getItem("user"));
 
     //if the user is not logged in, log the user out
     if (!user || !user.token) {
@@ -48,6 +49,18 @@ function App() {
       });
   });
 
+  //logic for loggin in the user or not
+  const onButtonClick = () => {
+    if(loggedIn)
+    {
+      localStorage.removeItem("user") //will remove the user from local storage
+      props.setLoggedIn(false) //will set loggedin status to false
+    }
+    else
+    {
+      Navigate("/login"); //if not logged in, navigate to the login page
+    }
+  }
   return (
     <div className="App">
       <header>
