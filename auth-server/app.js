0215c690cbbdc098f8f4e24d8e49f543b7ed3444
Isabella <bdc190799@gmail.com>
2023-11-09 12:53:09 +0000

    Added json web token verification
    Added json web token verification alongside methods to catch errors such as failed authentication and other unusual circumstances.

--- auth-server/app.js
+++ auth-server/app.js
@@ -17,7 +17,7 @@ const jwtSecretKey = "2df1644a6ee0666798271e42cc56e9c4"; //generated randomly, a
 //setting up cors and JWT
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //--- Endpoints ---
 
@@ -28,19 +28,17 @@ app.get("/", (_req, res) => {
 
 //creating a new user, or log in based on existing data
 app.post("/auth", (req, res) => {
-    const {email, password} = req.body; //the request body will contain the email and password data
+    const { email, password } = req.body; //the request body will contain the email and password data
 
     //verify if the user exists or not
     const user = db.get("users").value().filter(user => email === user.email); //using the user email as the key to search from
 
     //if the user exists, compare passwords
-    if(user.length === 1)
-    {
-        bcrypt.compare(password, user[0].password, function(_err, result) 
-        {
-            if(!result) //if password not correct
+    if (user.length === 1) {
+        bcrypt.compare(password, user[0].password, function (_err, result) {
+            if (!result) //if password not correct
             {
-                return res.status(401).json({message: "Invalid password"}); //error to be displayed on screen
+                return res.status(401).json({ message: "Invalid password" }); //error to be displayed on screen
             }
             else //if password is also correct
             {
@@ -49,15 +47,15 @@ app.post("/auth", (req, res) => {
                     signInTime: Date.now() //log current time of login
                 };
                 const token = jwt.sign(loginData, jwtSecretKey); //generates unique token to authenticate login request
-                res.status(200).json({message: "success", token}); //returns success status along with the unique token just generated
+                res.status(200).json({ message: "success", token }); //returns success status along with the unique token just generated
             }
         })
     }
-    else if(user.length === 0) //if no user found
+    else if (user.length === 0) //if no user found
     {
-        bcrypt.hash(password, 10, function (_err, hash){ //hash the password for the new user
-            console.log({email, password: hash}); //log user login to the console for verification
-            db.get("users").push({email, password: hash}).write(); //write the new user information to the database for future reference
+        bcrypt.hash(password, 10, function (_err, hash) { //hash the password for the new user
+            console.log({ email, password: hash }); //log user login to the console for verification
+            db.get("users").push({ email, password: hash }).write(); //write the new user information to the database for future reference
 
             let loginData = { //log the user login time
                 email,
@@ -65,7 +63,28 @@ app.post("/auth", (req, res) => {
             };
 
             const token = jwt.sign(loginData, jwtSecretKey);
-            res.status(200).json({mssage: "success", token});
+            res.status(200).json({ mssage: "success", token });
         });
+    }
+});
+
+//verifying the validity of the JWT
+app.post("/verify", (req, res) => {
+    const tokenHeaderKey = "jwt-token";
+    const authToken = req.headers[tokenHeaderKey];
+    try {
+        const isVerified = jwt.verify(authToken, jwtSecretKey); //will compare authentication token vs the secret key
+        if (isVerified) //if verification passes
+        {
+            return res.status(200).json({ status: "logged in", message: "success" });
+        }
+        else //unable to verify, deny access
+        {
+            return res.status(401).json({ status: "invalid auth", message: "error" });
+        }
+    }
+    catch (error) //if an error occurs, deny access
+    {
+        return res.status(401).json({ status: "invalid auth", message: "error" });
     }
 });
